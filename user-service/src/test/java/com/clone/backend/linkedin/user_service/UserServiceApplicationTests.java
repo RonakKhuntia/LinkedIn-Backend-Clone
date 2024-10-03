767870e66155c081d0565b0bad914674d498468b
RonakKhuntia <khuntiarounak@gmail.com>
2024-10-03 10:41:21 +0530

    added posts and users projects

---
+++ user-service/src/test/java/com/clone/backend/linkedin/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.backend.linkedin.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
