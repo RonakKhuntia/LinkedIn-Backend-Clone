c3dd24e33d6b12c17ce2e9486f1fa7a15ff3d912
RonakKhuntia <khuntiarounak@gmail.com>
2024-10-22 14:39:54 +0530

    added posts kafka producer config

--- posts-service/src/main/java/com/clone/backend/linkedin/posts_service/service/PostsService.java
+++ posts-service/src/main/java/com/clone/backend/linkedin/posts_service/service/PostsService.java
@@ -6,11 +6,13 @@ import com.clone.backend.linkedin.posts_service.dto.PersonDto;
 import com.clone.backend.linkedin.posts_service.dto.PostCreateRequestDto;
 import com.clone.backend.linkedin.posts_service.dto.PostDto;
 import com.clone.backend.linkedin.posts_service.entity.Post;
+import com.clone.backend.linkedin.posts_service.event.PostCreatedEvent;
 import com.clone.backend.linkedin.posts_service.exception.ResourceNotFoundException;
 import com.clone.backend.linkedin.posts_service.repository.PostsRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -24,23 +26,28 @@ public class PostsService {
     private final PostsRepository postsRepository;
     private final ModelMapper modelMapper;
     private final ConnectionsClient connectionsClient;
+    private final KafkaTemplate<Long, PostCreatedEvent> kafkaTemplate;
 
-    public PostDto createPost(PostCreateRequestDto postDto, Long userId) {
+    public PostDto createPost(PostCreateRequestDto postDto) {
+        Long userId = UserContextHolder.getCurrentUserId();
         Post post = modelMapper.map(postDto, Post.class);
         post.setUserId(userId);
 
         Post savedPost = postsRepository.save(post);
+
+        PostCreatedEvent postCreatedEvent = PostCreatedEvent.builder()
+                .postId(savedPost.getId())
+                .creatorId(userId)
+                .content(savedPost.getContent())
+                .build();
+
+        kafkaTemplate.send("post-created-topic", postCreatedEvent);
+
         return modelMapper.map(savedPost, PostDto.class);
     }
 
     public PostDto getPostById(Long postId) {
         log.debug("Retrieving post with ID: {}", postId);
-
-        Long userId = UserContextHolder.getCurrentUserId();
-
-        List<PersonDto> firstConnections = connectionsClient.getFirstConnections();
-
-//      TODO send Notifications to all connections
 
         Post post = postsRepository.findById(postId).orElseThrow(() ->
                 new ResourceNotFoundException("Post not found with id: "+postId));
