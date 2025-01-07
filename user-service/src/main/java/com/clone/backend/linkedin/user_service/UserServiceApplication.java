7ffb00dea982b23403b3f9111f44211378fad0e7
rounakkhuntia <khuntiarounak@gmail.com>
2025-01-07 22:56:09 +0530

    added connections service and implemented api gateway

--- user-service/src/main/java/com/clone/backend/linkedin/user_service/UserServiceApplication.java
+++ user-service/src/main/java/com/clone/backend/linkedin/user_service/UserServiceApplication.java
@@ -2,6 +2,7 @@ package com.clone.backend.linkedin.user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class UserServiceApplication {
